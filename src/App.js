6e0b2c07b9eced07a71cd607526b5ff27a5e28e2
redotomi <tomy.redondo001@gmail.com>
2022-08-04 18:21:01 -0300

    Agrega funcionalidad de lockear dados y de identificar si el jugador ganó

--- src/App.js
+++ src/App.js
@@ -1,35 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 import Die from "./components/Die";
 import RollButton from "./components/RollButton";
 import './style.css'
 
 export default function App() {
   const [dice, setDice] = useState(allNewDice());
+  const [rollCount, setRollCount] = useState(0)
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSame = dice.every(die => die.value === firstValue)
+    if (allHeld && allSame) {
+      setTenzies(true);
+      console.log('sos god amigo')
+    }
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 1; i <= 10; i++) {
-      const randomNumber = Math.ceil(Math.random() * 6);
-      const newDie = {
-        value: randomNumber,
-        isHeld: false
-      }
+      const newDie = generateNewDie()
       newDice.push(newDie);
     }
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map((die) => (
+      die.isHeld === true ?
+        die :
+        generateNewDie()
+    )))
+    setRollCount(prevCount => prevCount + 1)
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map((die) => (
+      die.id === id ?
+        {
+          ...die,
+          isHeld: !die.isHeld
+        } :
+        die
+    )))
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+      </p>
+      <p className="instructions count">
+        Count: {rollCount}
+      </p>
       <div className="die-container">
         {dice.map((die, i) => (
           <Die
             value={die.value}
-            key={i}
+            key={die.id}
+            isHeld={die.isHeld}
+            holdDice={() => { holdDice(die.id) }}
           />
         ))}
       </div>
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,7 +3,13 @@ import '../style.css'
 
 export default function Die(props) {
   return (
-    <div className="die">
+    <div
+      className="die"
+      style={{
+        backgroundColor: props.isHeld ? '#59E391' : 'white'
+      }}
+      onClick={props.holdDice}
+    >
       {props.value}
     </div>
   );
